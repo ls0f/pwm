08e771f25b6466d850a03ae9213271cac3825c6a
Je11yFish <lyj@cooyun.com>
2016-05-11 10:20:53 +0800

    fix bugs and add paramete 'batch' (#1)
    * fix bugs and add paramete 'batch'
    
    * fix typo in create_table
    
    * format help
    
    * remove debug codes

--- pwm/pwm.py
+++ pwm/pwm.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+#!coding:utf-8
 
 from hashlib import sha1
 import sys
@@ -9,6 +9,7 @@ import os
 import getpass
 from optparse import OptionParser
 
+
 __author = 'lovedb0y'
 __version = '0.1'
 __package = 'pwm'
@@ -16,11 +17,12 @@ __package = 'pwm'
 
 class PWM(object):
 
-    def __init__(self, key, db_path = None):
+    def __init__(self, key, db_path=None):
         self.key = key
         self.passwd_length = 15
         self.db_path = db_path
         self.table = 'pwm'
+        self._create_table()
 
     def gen_passwd(self, raw):
 
@@ -39,13 +41,15 @@ class PWM(object):
         list_passwd = list(passwd)
 
         if re.search(r"[0-9]", passwd) is None:
-            list_passwd[-3] = self.num_str[ord(passwd[-3])%len(self.num_str)]
+            list_passwd[-3] = self.num_str[ord(passwd[-3]) % len(self.num_str)]
 
         if re.search(r"[a-z]", passwd) is None:
-            list_passwd[-2] = self.low_letters[ord(passwd[-2])%len(self.low_letters)]
+            list_passwd[-2] = self.low_letters[ord(passwd[-2]) % len(
+                self.low_letters)]
 
         if re.search(r"[A-Z]", passwd) is None:
-            list_passwd[-1] = self.upper_letters[ord(passwd[-1])%len(self.upper_letters)]
+            list_passwd[-1] = self.upper_letters[ord(passwd[-1]) % len(
+                self.upper_letters)]
 
         return ''.join(list_passwd)
 
@@ -53,7 +57,12 @@ class PWM(object):
         if self.db_path is None:
             print "You didn't set you PWD_DB_PATH ENV"
             sys.exit(1)
-        conn = sqlite3.connect(self.db_path)
+        try:
+            conn = sqlite3.connect(self.db_path)
+        except sqlite3.OperationalError:
+            print "PWD_DB_PATH: %s is invalid! "\
+                    "(Is it a directory or a file?)" % self.db_path
+            sys.exit(1)
         return conn
 
     def __enter__(self):
@@ -72,32 +81,41 @@ class PWM(object):
         create table if not exists {}(
           `id` INTEGER PRIMARY KEY,
           `domain` varchar(32) ,
-          `account` varchar(32)
+          `account` varchar(32),
+          `batch` varchar(4) default NULL
           );
         """.format(self.table)
 
         with self:
             cur = self.conn.cursor()
             cur.execute(sql)
+            sql = "PRAGMA table_info(pwm);"
+            cur.execute(sql)
+            for cid, name, _, _, _, _ in cur.fetchall():
+                if name == "batch":
+                    return
+            else:
+                sql = "ALTER TABLE %s ADD COLUMN batch varchar(4);" % (
+                    self.table, )
+                cur.execute(sql)
 
-    def _insert_account(self, domain, account):
-
-        self._create_table()
-        sql = "insert into {} (domain, account) values ('{}', '{}');".format(self.table, domain, account)
+    def _insert_account(self, domain, account, batch=None):
+        sql = "insert into {} (domain, account, batch) values "\
+                "('{}', '{}', '{}');".format(
+                    self.table, domain, account, batch)
         with self:
             cur = self.conn.cursor()
             cur.execute(sql,)
 
     def _query_account(self, keyword):
-        self._create_table()
-        args = []
-
         if keyword:
-            query = " where domain like '%{}%' or account like '%{}%' ".format(keyword, keyword)
+            query = " where domain like '%{}%' or account like '%{}%' ".format(
+                keyword, keyword)
         else:
             query = ""
 
-        sql = "select id,domain,account from {} {}".format(self.table, query)
+        sql = "select id,domain,account,batch from {} {}".format(
+            self.table, query)
         # print sql
 
         with self:
@@ -108,29 +126,31 @@ class PWM(object):
 
     def _delete(self, id):
 
-        self._create_table()
         sql = "delete from {} where id={}".format(self.table, id)
         with self:
             cur = self.conn.cursor()
             raw_count = cur.execute(sql)
             return raw_count
 
-    def insert(self, domain, account):
-        self._insert_account(domain, account)
+    def insert(self, domain, account, batch):
+        self._insert_account(domain, account, batch)
         print "save success"
 
     @staticmethod
-    def gen_sign_raw(domain, account):
-        return "{}@{}".format(account, domain)
+    def gen_sign_raw(domain, account, batch):
+        raw = "{}@{}".format(account, domain)
+        if batch:
+            raw = "{}@{}".format(raw, str(batch))
+        return raw
 
-    def gen_account_passwd(self, domain, account):
+    def gen_account_passwd(self, domain, account, batch):
 
-        raw = self.gen_sign_raw(domain, account)
+        raw = self.gen_sign_raw(domain, account, batch)
         return self.gen_passwd(raw)
 
     def delete(self, id):
-        raw_count = self._delete(id)
-        print "remove success"
+        row_count = self._delete(id)
+        print "remove success, %s record(s) removed" % (row_count)
 
     def search(self, keyword):
 
@@ -138,12 +158,15 @@ class PWM(object):
             keyword = ''
 
         result = self._query_account(keyword)
-        print "ID    DOMAIN      ACCOUNT     PASSWORD"
+        fmt = "%-5s|%-20s|%-15s|%-20s|%-5s"
+        print fmt % ("ID", "DOMAIN", "ACCOUNT", "PASWORD", "BATCH")
+        print fmt % ("-"*5, "-"*20, "-"*15, "-"*20, "-"*5)
         for item in result:
-            line = "{}    {}    {}      {}".format(item[0], item[1], item[2], self.gen_account_passwd(item[1], item[2]))
-            print line
+            print fmt % (item[0], item[1], item[2],
+                         self.gen_account_passwd(item[1], item[2], item[3]),
+                         item[3])
 
-        print "A total of {} records".format(len(result))
+        print "\n{} records found.\n".format(len(result))
 
 
 def main():
@@ -157,12 +180,22 @@ def main():
         print "###############################"
     parse = OptionParser(version="{} {}".format(__package, __version))
 
-    parse.add_option('-k', '--key',help="your secret key", nargs=0)
-    parse.add_option('-d', '--domain',help="the domain of you account")
-    parse.add_option('-a', '--account',help="the account used to login")
-    parse.add_option('-s', '--search',help="list your account and domain by search keyword")
-    parse.add_option('-w', '--save',help="save your account and domain", nargs=0)
-    parse.add_option('-r', '--remove',help="remove your account and domain by id", nargs=1, type=int)
+    parse.add_option('-k', '--key', help="your secret key", nargs=0)
+    parse.add_option('-d', '--domain', help="the domain of you account")
+    parse.add_option('-a', '--account', help="the account used to login")
+    parse.add_option(
+        '-s', '--search',
+        help="list your account and domain by search keyword")
+    parse.add_option(
+        '-w', '--save',
+        help="save your account and domain", nargs=0)
+    parse.add_option(
+        '-r', '--remove',
+        help="remove your account and domain by id", nargs=1, type=int)
+    parse.add_option(
+        '-b', '--batch',
+        help="add batch to generate diffrent passwd with same domain and account",  # noqa
+        nargs=1, type=int)
     (options, args) = parse.parse_args()
 
     if options.key is not None:
@@ -187,17 +220,14 @@ def main():
         parse.print_help()
         return
 
-    # print "passwd:{}".format(pwm.gen_account_passwd(options.domain, options.account))
+    print "passwd is :\n{}".format(
+        pwm.gen_account_passwd(
+            options.domain, options.account, options.batch))
 
     # 保存
     if options.save is not None:
-        pwm.insert(options.domain, options.account)
-
-
-
+        pwm.insert(options.domain, options.account, options.batch)
 
 
 if __name__ == "__main__":
-
     main()
-
